e0914d851202f05e0d274faba03f0aabb0ceee3f
Patrick Ruiz <patrickruiz@0e:a5:37:b2:cd:8f.local.ftr>
2026-07-29 16:37:49 -0700

    feat: clear app badge and delivered notifications when app opens
    Clears badge count to 0 and removes all delivered notifications
    from notification center in applicationDidBecomeActive

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -44,7 +44,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Clear badge count and delivered notifications when user opens the app
+        application.applicationIconBadgeNumber = 0
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
